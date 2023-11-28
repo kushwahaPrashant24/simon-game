ceee064a7eeab554950b11dfc418aafe48a7f83f
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2023-11-28 23:40:13 +0530

    added sound keyfetch detection and aniation in the game

--- script.js
+++ script.js
@@ -4,27 +4,83 @@
  var buttonColours = ["red", "blue", "green", "yellow"];
 
  var gamePattern = [];
+
+ var userClickedPattern = [];
+
+ /*var btnclick = document.querySelectorAll(".btn").length;
+
+for ( var i = 0; i <= btnclick; i++) {
+
+
+document.querySelectorAll(".btn")[i].addEventListener("click", function() {
+    
+  var userChosenColour = $(this).attr("id");
+
+  userClickedPattern.push(userChosenColour);
+  
+  console.log(userClickedPattern);
+  
+
+
+
+});
+ 
+}*/
+
+
+
+
+
+$(".btn").click( function(){
+   var userChosenColour = $(this).attr("id");
+
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+});
+
+
  
 function nextSequence() {
      
     var randomnumber = Math.floor(Math.random() * 4);
 
-    var randomChosenColour = buttonColours[randomNumber];
+    var randomChosenColour = buttonColours[randomnumber];
 
     gamePattern.push(randomChosenColour);
 
 
-    $("#" +randomChosenColour).animate({
-      height: '250px',
-      width: '250px'
-    }).fadeIn("2000");
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    var soundplay = new Audio("sound/"+ randomChosenColour +".mp3");
-    soundplay.play(); 
+    //var soundplay = new Audio("sound/"+ randomChosenColour +".mp3");
+    //soundplay.play(); 
+    playSound(randomChosenColour);
  }
 
 
-$(".btn").on("click", function (){
-  alert("hi")
-});
- 
+ function playSound(name) {
+  var soundplay = new Audio("sounds/"+ name +".mp3");
+  soundplay.play(); 
+ }
+
+ function animatePress(currentColour) {
+
+  $("#" + currentColour).addClass("pressed");
+
+  setTimeout(
+    function(){
+      $("#" + currentColour).removeClass("pressed");
+    }, 100
+  );
+
+ }
+
+ $(document).keypress(function(){
+  
+
+ }
+
+ );
